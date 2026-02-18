e6c043aacade83a0744bb51aaa5e4112d3bde069
carlos lucero <carlosluceror@gmail.com>
2026-02-18 16:36:50 +0100

    ci: configure static export and GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/insight' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/insight/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
